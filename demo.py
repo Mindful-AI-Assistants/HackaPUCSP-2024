01121b68b116e8c3f2d7a2350858e61867f73d2c
Fabiana Campanari <fabicampanari@gmail.com>
2024-03-05 17:13:04 -0300

    demo.py
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 
 def main():
     print("Seja muito bem-vindo a este repositório, desenvolvido pelos alunos de Ciência de Dados e Inteligência Artificial da PUC-SP, pertencentes ao grupo 5 do HACKAPUC-SP 2ª edição, que conquistaram o 1º lugar na competição de fevereiro de 2024!")
-    print("")
+    print("ste é o meu arquivo de demonstração em Python.")
     print("Ele contém código relacionado à 2ª edição do HACKAPUC-SP.")
     print("Sinta-se à vontade para explorar e contribuir!")
 
