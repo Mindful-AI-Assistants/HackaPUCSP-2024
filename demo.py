f898e2751eccbd3bff46bfa466e46917284f1546
Fabiana Campanari <fabicampanari@gmail.com>
2024-03-05 17:02:02 -0300

    Update demo.py
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- demo.py
+++ demo.py
@@ -1,1 +1,9 @@
+# demo.py
+
+def main():
+    print("Ele contém código relacionado à 2ª edição do HACKAPUC-SP.")
+    print("Sinta-se à vontade para explorar e contribuir!")
+
+if __name__ == "__main__":
+    main()
 
