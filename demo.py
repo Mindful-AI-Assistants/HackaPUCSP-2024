d127e66fc38a610a61f6d415629f01928aa76f29
Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2024-04-11 21:55:58 -0300

    demo.py
    Signed-off-by: Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>

--- demo.py
+++ demo.py
@@ -1,6 +1,17 @@
 # demo.py
 
 def main():
+    print("Welcome to this repository, developed by the students of Data Science and Artificial Intelligence from PUC-SP, belonging to group 5 of HACKAPUC-SP 2nd edition, who won the 1st place in the competition of February 2024!")
+    print("This is my demonstration file in Python.")
+    print("It contains code related to the MindfulAl-Copilots-Bots HACKAPUC-SP.")
+    print("Feel free to explore and contribute!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+def main():
     print("Seja muito bem-vindo a este repositório, desenvolvido pelos alunos de Ciência de Dados e Inteligência Artificial da PUC-SP, pertencentes ao grupo 5 do HACKAPUC-SP 2ª edição, que conquistaram o 1º lugar na competição de fevereiro de 2024!")
     print("ste é o meu arquivo de demonstração em Python.")
     print("Ele contém código relacionado à 2ª edição do HACKAPUC-SP.")
